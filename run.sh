06b48b4072ea4285225bac0456dface28f953ede
Alexander Novikov <mem-memov@yandex.ru>
2019-02-28 16:34:31 +0300

    running with errors

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-set -e -u
-
 ATTEMPT=1
 
 set
 while [[ "${ATTEMPT}" -lt 100 ]]; do
 
-    gcc main.c -o main && ./main &
-
     ATTEMPT=$(( ${ATTEMPT} + 1 ))
 
+    gcc main.c -o main && ./main &
+
 done
+
+wait 
+echo "All done"
