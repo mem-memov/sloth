f5cfe325a6f5a387bf1c18bcbaed87c4e70e972f
Alexander Novikov <mem-memov@yandex.ru>
2019-02-28 16:25:30 +0300

    running 100 in parallel

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ set -e -u
 ATTEMPT=1
 
 set
-while [[ "${ATTEMPT}" -lt 2 ]]; do
+while [[ "${ATTEMPT}" -lt 100 ]]; do
 
-    gcc main.c -o main && ./main
+    gcc main.c -o main && ./main &
 
     ATTEMPT=$(( ${ATTEMPT} + 1 ))
 
