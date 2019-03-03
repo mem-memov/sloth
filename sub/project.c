034cc6ff3102bec1d4292f0eda5b23c9d0fc7e0b
Alexander Novikov <mem-memov@yandex.ru>
2019-03-03 12:25:18 +0300

    namespaces

--- sub/project.c
+++ sub/project.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include "../call.c"
 
+char deepProject[] = "sub/deep/project";
+
 int main(int argc, char *argv[])
 {
     printf("I am projecting from sub space!\n");
-    static char * fn[] = {"sub/deep/project", NULL};
+
+    static char * fn[] = {deepProject, NULL};
     call(fn);
+
     return 0;
 }
