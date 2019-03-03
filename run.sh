60ccec01d4af7ff0d6421532ab142d13c705af7c
Alexander Novikov <mem-memov@yandex.ru>
2019-03-03 11:05:38 +0300

    1000 processes

--- run.sh
+++ run.sh
@@ -4,13 +4,15 @@ gcc main.c -o main
 
 ATTEMPT=1
 
-while [[ "${ATTEMPT}" -lt 100 ]]; do
+while [[ "${ATTEMPT}" -lt 1000 ]]; do
+
+    echo ${ATTEMPT}
 
     ATTEMPT=$(( ${ATTEMPT} + 1 ))
 
     ./main &
 
-    sleep 1
+    #sleep 1
 
 done
 
